b7349801817762adf2a6bc6081f30b961738626a
Kevin <kev776522.mg08@nycu.edu.tw>
2025-10-10 17:23:29 +0800

    fix: resolve the data inconsistent issue and update docker image version

--- crawler.js
+++ crawler.js
@@ -1,29 +1,35 @@
-const axios = require('axios');
-const amqp = require('amqplib/callback_api');
+const axios = require("axios");
+const amqp = require("amqplib/callback_api");
 
-module.exports = amqp.connect('amqp://my-rabbitmq', function(error, connection) {
-  if (error) {
-    console.log(error);
-  }
-  connection.createChannel(function(error, channel) {
+module.exports = amqp.connect(
+  "amqp://my-rabbitmq",
+  function (error, connection) {
     if (error) {
       console.log(error);
     }
-    const queue = 'crawler2';
-    channel.assertQueue(queue, {
-      durable: false
-    });
-    setInterval(() => {
-      axios.get('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
-      .then((response) => {
-        for(let i=0; i<response.data.length; i++) {
-          const msg = JSON.stringify(response.data[i]);
-          channel.sendToQueue(queue, Buffer.from(msg));
-        }
-      }).catch(error => {
+    connection.createChannel(function (error, channel) {
+      if (error) {
         console.log(error);
+      }
+      const queue = "crawler2";
+      channel.assertQueue(queue, {
+        durable: false,
       });
-    }, 60000);
-  });
-});
-
+      setInterval(() => {
+        axios
+          .get(
+            "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+          )
+          .then((response) => {
+            for (let i = 0; i < response.data.length; i++) {
+              const msg = JSON.stringify(response.data[i]);
+              channel.sendToQueue(queue, Buffer.from(msg));
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, 10000);
+    });
+  }
+);
--- receiver.js
+++ receiver.js
@@ -1,39 +1,51 @@
-const amqp = require('amqplib/callback_api');
+const amqp = require("amqplib/callback_api");
 const { MongoClient } = require("mongodb");
 
-module.exports = amqp.connect('amqp://my-rabbitmq', function(error, connection) {
+module.exports = amqp.connect(
+  "amqp://my-rabbitmq",
+  function (error, connection) {
     if (error) {
-        console.log(error);
+      console.log(error);
     }
-    connection.createChannel(async function(error, channel) {
-        if (error) {
-            console.log(error);
+    connection.createChannel(async function (error, channel) {
+      if (error) {
+        console.log(error);
+      }
+      // mongodb connection
+      const uri = "mongodb://my-mongodb:27017";
+      const client = new MongoClient(uri);
+      await client.connect();
+      const database = client.db("demo");
+      const youbike = database.collection("youbike");
+      // rabbitmq
+      const queue = "crawler2";
+      channel.assertQueue(queue, {
+        durable: false,
+      });
+      channel.consume(
+        queue,
+        function (msg) {
+          channel.ack(msg);
+          const receiveMsg = JSON.parse(msg.content.toString());
+          const finalMsg = {
+            station: receiveMsg.sna,
+            version: "v2",
+            available: parseInt(receiveMsg.available_rent_bikes),
+            location: {
+              type: "Point",
+              coordinates: [
+                parseFloat(receiveMsg.longitude),
+                parseFloat(receiveMsg.latitude),
+              ],
+            },
+            datatime: receiveMsg.mday,
+          };
+          youbike.insertOne(finalMsg);
+        },
+        {
+          noAck: false,
         }
-        // mongodb connection
-        const uri = "mongodb://my-mongodb:27017";
-        const client = new MongoClient(uri);
-        await client.connect();
-        const database = client.db("demo");
-        const youbike = database.collection("youbike");
-        // rabbitmq 
-        const queue = 'crawler2';
-        channel.assertQueue(queue, {
-            durable: false
-        });
-        channel.consume(queue, function(msg) {
-            channel.ack(msg);
-            const receiveMsg = JSON.parse(msg.content.toString());
-            const finalMsg = {
-                'station': receiveMsg.sna,
-                'version': 'v2',
-                'available': parseInt(receiveMsg.sbi),
-                'location': {'type': 'Point', 'coordinates': [parseFloat(receiveMsg.lng), parseFloat(receiveMsg.lat)]},
-                'datatime': receiveMsg.mday,
-            };
-            youbike.insertOne(finalMsg);
-        }, {
-            noAck: false
-        });
+      );
     });
-});
-
+  }
+);
