401cd4a21fd99dd687c27b33f9bad711c894fa5f
Kevin <kev776522.mg08@nycu.edu.tw>
2025-10-22 16:43:45 +0800

    feat(helm): update Kubernetes templates for dynamic host configuration

--- crawler.js
+++ crawler.js
@@ -1,35 +1,34 @@
 const axios = require("axios");
 const amqp = require("amqplib/callback_api");
+const rabbitmqHost = process.env.RABBITMQ_HOST || "localhost";
+const rabbitmqUrl = `amqp://${rabbitmqHost}:5672`;
 
-module.exports = amqp.connect(
-  "amqp://my-rabbitmq",
-  function (error, connection) {
+module.exports = amqp.connect(rabbitmqUrl, function (error, connection) {
+  if (error) {
+    console.log(error);
+  }
+  connection.createChannel(function (error, channel) {
     if (error) {
       console.log(error);
     }
-    connection.createChannel(function (error, channel) {
-      if (error) {
-        console.log(error);
-      }
-      const queue = "crawler2";
-      channel.assertQueue(queue, {
-        durable: false,
-      });
-      setInterval(() => {
-        axios
-          .get(
-            "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
-          )
-          .then((response) => {
-            for (let i = 0; i < response.data.length; i++) {
-              const msg = JSON.stringify(response.data[i]);
-              channel.sendToQueue(queue, Buffer.from(msg));
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }, 10000);
+    const queue = "crawler2";
+    channel.assertQueue(queue, {
+      durable: false,
     });
-  }
-);
+    setInterval(() => {
+      axios
+        .get(
+          "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+        )
+        .then((response) => {
+          for (let i = 0; i < response.data.length; i++) {
+            const msg = JSON.stringify(response.data[i]);
+            channel.sendToQueue(queue, Buffer.from(msg));
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 10000);
+  });
+});
--- util.js
+++ util.js
@@ -1,11 +1,10 @@
 const { MongoClient } = require("mongodb");
+const mongodbHost = process.env.MONGODB_HOST || "localhost";
+const mongodbUrl = `mongodb://${mongodbHost}:27017`;
 
 async function getData(ver, lng, lat) {
-  // Connection URI
-  const uri =
-  "mongodb://my-mongodb:27017";
   // Create a new MongoClient
-  const client = new MongoClient(uri);
+  const client = new MongoClient(mongodbUrl);
 
   try {
     // Connect the client to the server
@@ -14,25 +13,31 @@ async function getData(ver, lng, lat) {
     // read data from mongodb
     const database = client.db("demo");
     const youbike = database.collection("youbike");
-    await youbike.createIndex( { location: "2dsphere" } );
-    let response = await youbike.aggregate( [
-      {
-         $geoNear: {
-            near: { type: "Point", coordinates: [ lng, lat ] },
+    await youbike.createIndex({ location: "2dsphere" });
+    let response = await youbike
+      .aggregate([
+        {
+          $geoNear: {
+            near: { type: "Point", coordinates: [lng, lat] },
             spherical: true,
             maxDistance: 2000,
             query: { version: ver },
-            distanceField: "calcDistance"
-         }
-      }, { $limit: 5 }
-    ] ).toArray();
-    if(response.length === 0) {
-      return {data: 'no station within 2km'};
+            distanceField: "calcDistance",
+          },
+        },
+        { $limit: 5 },
+      ])
+      .toArray();
+    if (response.length === 0) {
+      return { data: "no station within 2km" };
     }
     const station = response[0].station;
-    response = await youbike.find({station:station, version: ver}).sort({datatime: -1}).toArray();
-    return {"data": response[0]};
-  } catch(error) {
+    response = await youbike
+      .find({ station: station, version: ver })
+      .sort({ datatime: -1 })
+      .toArray();
+    return { data: response[0] };
+  } catch (error) {
     console.log(error);
     return;
   } finally {
@@ -41,5 +46,5 @@ async function getData(ver, lng, lat) {
 }
 
 module.exports = {
-  getData
-}
+  getData,
+};
